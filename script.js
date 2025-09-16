68d4d10c94a2433428134d5a5c4cbc3d92d711e1
Lucas Coutinho <lucaspp1coutinho@gmail.com>
2025-09-16 16:56:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -332,6 +332,6 @@ const dePara = [
 { AUXILIAR:"URA LIBERAÇÃO",  ramal: "3640",  usuario: "URA", status: "ATIVADO", setor: "LIBERAÇÃO"},
 { AUXILIAR:"VANESSA AREA TECNICA",  ramal: "3617",  usuario: "VANESSA TECNICA", status: "ATIVO", setor: "AREA TECNICA"},
 { AUXILIAR:"EDRYELLEN COMERCIAL",  ramal: "3607",  usuario: "EDRYELLEN COMERCIAL", status: "ATIVO", setor: "COMERCIAL"}, 
-{ AUXILIAR:"VILSON COMERCIAL",  ramal: "3608",  usuario: "VILSON COMERCIAL", status: "ATIVO", setor: "COMERCIAL"
+{ AUXILIAR:"VILSON COMERCIAL",  ramal: "3608",  usuario: "VILSON COMERCIAL", status: "ATIVO", setor: "COMERCIAL"}
 
 }
